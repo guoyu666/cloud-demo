89da9c40c25eb3f73501079f51850617c634f2f3
GuoYu <1262425287@qq.com>
2024-09-08 15:39:18 +0800

    使用Feign远程调用，将之前使用的nacos集群模式改为单机模式，nacos注册地址切换为loacalhost:8848

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -1,5 +1,6 @@
 package cn.itcast.order.service;
 
+import cn.itcast.order.clients.UserClient;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
@@ -14,6 +15,21 @@ public class OrderService {
     private OrderMapper orderMapper;
 
     @Autowired
+    private UserClient userClient;
+
+    public Order queryOrderById(Long orderId) {
+        // 1. 查询订单
+        Order order = orderMapper.findById(orderId);
+        // 2. 用Feign远程调用
+        User user = userClient.fingById(order.getUserId());
+        // 3. 封装user到Order对象中
+        order.setUser(user);
+        // 4.返回
+        return order;
+    }
+
+
+/*    @Autowired
     private RestTemplate restTemplate;
 
     public Order queryOrderById(Long orderId) {
@@ -28,5 +44,5 @@ public class OrderService {
         order.setUser(user);  // 注意：Order类中具有User类型的字段，这与数据库中的字段不一致
         // 4.返回
         return order;
-    }
+    }*/
 }
